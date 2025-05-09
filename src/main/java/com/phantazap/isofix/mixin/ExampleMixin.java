b13c11242daf36e43e44b0cdeef52ed5b2138099
Phantazap <46830523+Phantazap@users.noreply.github.com>
2025-05-09 12:14:51 -0500

    Initial commit

---
+++ src/main/java/com/phantazap/isofix/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.phantazap.isofix.mixin;
+
+import net.minecraft.client.C_5664496;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(C_5664496.class) // C_5664496 = MinecraftClient
+public class ExampleMixin {
+	@Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GameGui;<init>(Lnet/minecraft/client/C_5664496;)V"))
+	private void init(CallbackInfo ci) {
+		// This code is injected after GameGui has been initialized in MinecraftClient.run()V
+		System.out.println("BAZINGA");
+	}
+}
